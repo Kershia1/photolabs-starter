509c68e733204e40032715c7d98330aa3814f0cf
Kershia1 <insatiable.meerkat@gmail.com>
2024-01-30 16:53:16 -0800

    Added styling to PhotoListItem img

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,9 +3,17 @@ import "../styles/PhotoListItem.scss";
 
 //pass as props to PhotoListItem
 const PhotoListItem = ({ id, location, imageSource, username, profile }) => {
+  const styledPhotoListItem = {
+    borderRadius: '8px 8px 8px 8px',
+    userSelect: 'none',
+    width: '300px',
+    height: '200px',
+    objectit: 'cover',
+  };
+
   return (
     <section className="photo-list__item ">
-      <img className="photo-list__image" src={imageSource} alt={username}></img>
+      <img className="photo-list__image" src={imageSource} alt={username} style={styledPhotoListItem}></img>
       <div className="photo-list__user-details"></div>
       <img src={profile} />
       <div className="photo-list__user-info" id={id}></div>
