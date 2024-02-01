5d35ea82edf62881981709c3b11536b1ccca38a5
Kershia1 <insatiable.meerkat@gmail.com>
2024-01-31 22:28:20 -0800

    FIX app.jsx consumes mock data, passes to children

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import HomeRoute from 'routes/HomeRoute';
+import photos from 'mocks/photos';
+import topics from 'mocks/topics';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
@@ -7,7 +9,7 @@ const App = () => {
   return (
     <div className="App">
       {/* <div className={`App ${dark}`}></div> */}
-      <HomeRoute />
+      <HomeRoute photos={photos} topics={topics} />
     </div>
   );
 };
